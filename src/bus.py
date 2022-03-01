c2416c88834f1b0fc5c17e106015177b5221877a
CordiiT14 <cordelia.toennies@live.com>
2022-03-01 14:33:50 +0000

    initial commit

---
+++ src/bus.py
@@ -0,0 +1,9 @@
+class Bus:
+
+    def __init__(self, route_num, destination):
+        self.route_num = route_num
+        self.destination = destination
+
+    def drive(self):
+        return "Brum brum"
+        
